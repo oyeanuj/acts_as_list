6e52a395e288ee4d44eb490036f5c9e6581b0883
Anuj <oyeanuj@users.noreply.github.com>
2018-07-27 20:28:47 -0700

    Adding print statements to debug

--- lib/acts_as_list/active_record/acts/active_record.rb
+++ lib/acts_as_list/active_record/acts/active_record.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
-
+puts "Before the on_load method in active_record.rb"
 ActiveSupport.on_load :active_record do
+  puts "In the on_load method in active_record.rb"
   extend ActiveRecord::Acts::List::ClassMethods
+  puts "After the extend command"
 end
+puts "After the on_load method in active_record.rb"
